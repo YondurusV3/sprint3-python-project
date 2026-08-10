af22b2080f7fe52de7534dc9e2d34a8e5b7ba6bd
Skylar McClure <91846482+YondurusV3@users.noreply.github.com>
2026-08-10 13:55:05 -0400

    task4_loops.py

---
+++ task4_loops.py
@@ -0,0 +1,32 @@
+# a) Print games with more than 25 million global sales
+for game in video_game_sales:
+    if game[GLOBAL_SALES] > 25:
+        print(game[NAME], game[GLOBAL_SALES])
+# b) Count games released before 2000
+pre_2000_count = 0
+for game in video_game_sales:
+    if game[YEAR] < 2000:
+        pre_2000_count += 1
+print("Games released before 2000:", pre_2000_count)
+# c) Calculate total NA and JP sales
+total_na_sales = 0
+total_jp_sales = 0
+
+for game in video_game_sales:
+    total_na_sales += game[NA_SALES]
+total_jp_sales += game[JP_SALES]
+print("Total North America sales:", total_na_sales)
+print("Total Japan sales:", total_jp_sales)
+if total_na_sales > total_jp_sales:
+    print("North America had higher sales.")
+elif total_jp_sales > total_na_sales:
+    print("Japan had higher sales.")
+else:
+    print("Sales were equal.")
+# d) Create a list of Nintendo games
+nintendo_games = []
+for game in video_game_sales:
+    if game[PUBLISHER] == 'Nintendo':
+        nintendo_games.append(game[NAME])
+print(nintendo_games)
+print("Number of Nintendo games:", len(nintendo_games))
